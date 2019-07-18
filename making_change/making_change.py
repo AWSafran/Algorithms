c96a3f8d12a5e41395eae68d43129390a96b1d62
AWSafran <andrewsafran@gmail.com>
2019-07-18 15:34:19 -0400

    naive solution to making change completed

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,34 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+
+  print(f"Running function for {amount}")
+
+  cache = [0] * (amount + 1)
+
+  def making_change_recursive(amount, denominations):
+    nonlocal cache
+    
+    if amount < 5:
+      return 1
+    else:
+
+      running_total = 0
+      for i in range(len(denominations)):
+        coin = denominations[i]
+
+        if amount >= coin:
+          new_denoms = []
+
+          for num in denominations:
+            if num <= coin:
+              new_denoms.append(num)
+          running_total += making_change_recursive(amount - coin, new_denoms)
+
+
+    return running_total
+
+  return making_change_recursive(amount, denominations)
 
 
 if __name__ == "__main__":
